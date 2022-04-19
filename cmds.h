5fc8e7dc1cd35bbaa7b79f767319076424ca59fb
Chugunov Roman <chugunovrv@lad24.ru>
2022-04-19 09:39:38 +0300

    feat: Add base shortcuts

---
+++ cmds.h
@@ -0,0 +1,40 @@
+// Declared commands for DWM
+
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
+/* rofi menu commands */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[]      	 	 = { "rofi", "-show", "run", NULL };
+static const char *dmenuClipCmd[]      = { "rofi", "-theme", "arc-red-dark", "-modi", "clipboard:greenclip print", "-show", "clipboard", "-run-command", "{cmd}", NULL };
+static const char *dmenuRecentCmd[]    = { "rofi", "-show", "run", "-theme", "arc-red-dark", NULL };
+
+/* run app commands */
+static const char *termcmd[]       		 = { "terminator", NULL };
+static const char *googlechromecmd[]   = { "google-chrome-stable", NULL };
+static const char *audaciouscmd[]  		 = { "audacious", NULL };
+static const char *vscodecmd[]     		 = { "code", NULL };
+static const char *nemocmd[]       		 = { "nemo", "--no-desktop", NULL };
+
+/* system commands */
+// TODO: rewrite on use bash scripts for take screenshots
+static const char *gsacmd[]       		 = { "shutter", "-s", NULL };
+static const char *gswcmd[]       		 = { "shutter", "-w", NULL };
+static const char *gsfcmd[]       		 = { "shutter", "-f", NULL };
+static const char *gsfccmd[]       		 = { "gnome-screenshot", "-a", NULL };
+
+static const char *xkillcmd[]	         = { "xkill", NULL };
+static const char *slock[]	  	       = { "slock", NULL };
+
+// Scripts
+// TODO: Update scripts
+static const char *toggleTouchpad[]    = { "bash", HOME"/sh/toggleTouchPad.sh", NULL };
+static const char *toggleTouchScreen[] = { "bash", HOME"/sh/toggleTouchScreen.sh", NULL };
+
+/* audacious commands */
+static const char *toggleAudacious[]   = { "audacious", "-t", NULL };
+static const char *nextAudacious[]     = { "audacious", "-f", NULL };
+static const char *rewAudacious[]      = { "audacious", "-r", NULL };
+
+/* commands for buttons */
+static const char *wallpaper[]	  	   = { "wallpaper.sh", NULL };
--- config.h
+++ config.h
@@ -1,3 +1,5 @@
+#include "cmds.h"
+
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
@@ -45,40 +47,53 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define ALT Mod1Mask
+#define MODKEY Mod4Mask
+#define SHIFT ShiftMask
+#define CTRL ControlMask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
-/* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
-
-/* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
-
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+
+	/* rofi menu shortcuts */
+	{ ALT,  	                      XK_p,      spawn,          {.v = dmenucmd } },
+	{ ALT,  	                      XK_c,      spawn,          {.v = dmenuClipCmd } },
+	{ ALT,  	                      XK_r,      spawn,          {.v = dmenuRecentCmd } },
+
+	/* run app shortcuts */
+	{ ALT,						              XK_t, 		 spawn,          {.v = termcmd } },
+	{ ALT,						              XK_w, 		 spawn,          {.v = googlechromecmd } },
+	{ ALT,						              XK_a, 		 spawn,          {.v = audaciouscmd } },
+	{ ALT,						              XK_x, 		 spawn,          {.v = vscodecmd } },
+	{ ALT,						              XK_e, 		 spawn,          {.v = nemocmd } },
+
+	/* system shortcuts */
+	{ MODKEY ,				              XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_x,	   	 spawn,	       	 {.v = xkillcmd } },
+
+	/* layouts */
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+
+	/* dwm shortcuts */
+	{ MODKEY,                       XK_space,  togglebar,      {0} },
+	{ MODKEY,                       XK_s,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_w,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_o,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_a,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_d,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY|SHIFT,             		XK_b,  		 togglefloating, {0} },
+
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
@@ -94,7 +109,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|CTRL|SHIFT,             XK_q,      quit,           {0} },
 };
 
 /* button definitions */
