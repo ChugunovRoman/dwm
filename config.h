ab11f1d110b90c5faa2cb2c52d0d4c2813c0a763
Chugunov Roman <chugunovrv@lad24.ru>
2022-05-29 23:34:43 +0300

    feat: Add support set border width for each window

--- config.h
+++ config.h
@@ -41,9 +41,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      instance    title       tags mask     border width    isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            borderpx,       1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       borderpx,       0,           -1 },
 };
 
 #include "funcions.h"
